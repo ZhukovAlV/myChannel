3c9ae4ead044926e7c72840bc6c2ac158143c30e
avzhykov@gmail.com <20122012>
2020-10-31 20:54:09 +0600

    Initial commit

---
+++ src/main/java/com/example/myChannel/MyChannelApplication.java
@@ -0,0 +1,13 @@
+package com.example.myChannel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChannelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyChannelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myChannel/MyChannelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myChannel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChannelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
